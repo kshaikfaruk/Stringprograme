d40d13d8201f9872c3de7b27c45f950c347abe68
shaikfaruk <shaikfaruk04@gmail.com>
2022-06-10 14:24:04 +0530

    testcase2

---
+++ src/Programe/Lengtgofstring.java
@@ -0,0 +1,15 @@
+package Programe;
+
+public class Lengtgofstring {
+public static void main(String[]args){
+	String s="Althaf";
+	int count=0;
+	
+	for(char c:s.toCharArray()){
+		count++;
+		
+	}
+	System.out.println(count);
+	
+}
+}
