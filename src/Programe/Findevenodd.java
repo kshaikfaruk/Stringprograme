cd46a3ece9aa0b06891a43963ebdb6c45ee6dd6d
shaikfaruk <shaikfaruk04@gmail.com>
2022-06-10 14:32:27 +0530

    testcase3

---
+++ src/Programe/Findevenodd.java
@@ -0,0 +1,18 @@
+package Programe;
+
+public class Findevenodd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int a[]={1,2,3,4,5,6,7,};
+       for(int i=0;i<a.length;i++){
+    	   if(a[i]%2==0){
+    		   System.out.println("evennunber"+a[i]);
+    	   }else{
+    		   System.out.println("oddnumber"+a[i]);
+    	   }
+       }
+      
+	}
+
+}
